81c00d79ee83ef76fa1e66afb0a6c80f3410639d
Joonamo <joonamo@users.noreply.github.com>
2021-04-25 15:16:38 +0300

    Cars and cliffs!

--- Assets/Objects/ObjectManager.cs
+++ Assets/Objects/ObjectManager.cs
@@ -4,40 +4,64 @@ using UnityEngine;
 
 public class ObjectManager : MonoBehaviour
 {
-    GameManager gm;
-    public GameObject leftWall;
-    public GameObject rightWall;
+  GameManager gm;
+  public GameObject leftWall;
+  public GameObject rightWall;
 
-    public List<GameObject> dangerObjects = new List<GameObject>();
-    public float minTime = 1.0f;
-    public float maxTime = 3.0f;
-    public float timeToNext = 5.0f;
+  public List<GameObject> dangerObjects = new List<GameObject>();
+  public float dangerMinTime = 1.0f;
+  public float dangerMaxTime = 3.0f;
+  public float timeToNextDanger = 5.0f;
 
-    // Start is called before the first frame update
-    void Start()
+  public List<GameObject> cliffObjects = new List<GameObject>();
+  public float cliffMinTime = 0.4f;
+  public float cliffMaxTime = 4.0f;
+  public float timeToNextCliff = 1.0f;
+
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    gm = GameObject.FindObjectOfType<GameManager>();
+
+    for (int i = -1; i <= 1; i++)
     {
-        gm = GameObject.FindObjectOfType<GameManager>();
+      GameObject.Instantiate(leftWall, new Vector3(gm.screenXMax, gm.screenYMax * i * 2, 0.0f), Quaternion.identity);
+      GameObject.Instantiate(rightWall, new Vector3(-gm.screenXMax, gm.screenYMax * i * 2, 0.0f), Quaternion.identity);
+    }
+  }
 
-        for (int i = -1; i <= 1; i++) {
-            GameObject.Instantiate(leftWall, new Vector3(gm.screenXMax, gm.screenYMax * i * 2, 0.0f), Quaternion.identity);
-            GameObject.Instantiate(rightWall, new Vector3(-gm.screenXMax, gm.screenYMax * i * 2, 0.0f), Quaternion.identity);
-        }
+  // Update is called once per frame
+  void Update()
+  {
+    timeToNextDanger = timeToNextDanger - Time.deltaTime;
+    if (timeToNextDanger <= 0.0f)
+    {
+      GameObject toSpawn = dangerObjects[Random.Range(0, dangerObjects.Count - 1)];
+      var newObject = GameObject.Instantiate(toSpawn, new Vector3(0.0f, -999.0f, 0.0f), Quaternion.identity);
+      float xExtent = gm.screenXMax - newObject.transform.lossyScale.x * 0.5f;
+      newObject.transform.position = new Vector3(
+          Random.Range(-xExtent, xExtent),
+          -gm.screenYMax - newObject.transform.lossyScale.y,
+          0.0f);
+      float r = Random.Range(0.0f, 1.0f);
+      timeToNextDanger = Mathf.Lerp(dangerMinTime, dangerMaxTime, r * r);
     }
 
-    // Update is called once per frame
-    void Update()
+    timeToNextCliff = timeToNextCliff - Time.deltaTime;
+    if (timeToNextCliff <= 0.0f)
     {
-        timeToNext = timeToNext - Time.deltaTime;
-        if (timeToNext <= 0.0f) {
-            GameObject toSpawn = dangerObjects[Random.Range(0, dangerObjects.Count - 1)];
-            var newObject = GameObject.Instantiate(toSpawn, new Vector3(0.0f, -999.0f, 0.0f), Quaternion.identity);
-            float xExtent = gm.screenXMax - newObject.transform.lossyScale.x * 0.5f;
-            newObject.transform.position = new Vector3(
-                Random.Range(-xExtent, xExtent),
-                -gm.screenYMax - newObject.transform.lossyScale.y * 0.5f,
-                0.0f);
+      GameObject toSpawn = cliffObjects[Random.Range(0, cliffObjects.Count - 1)];
+      var newObject = GameObject.Instantiate(toSpawn, new Vector3(0.0f, -999.0f, 0.0f), Quaternion.identity);
+      float side = Random.Range(-1.0f, 1.0f) < 0.0f ? -1.0f : 1.0f;
+      var extent = newObject.transform.lossyScale;
+      newObject.transform.position = new Vector3(
+          side * gm.screenXMax - side * extent.x * 0.5f, //Random.Range(0.0f, extent.x * 0.7f),
+          -gm.screenYMax - extent.y * 0.5f,
+          0.0f);
 
-            timeToNext = Random.Range(minTime, maxTime);
-        }
+      float r = Random.Range(0.0f, 1.0f);
+      timeToNextCliff = Mathf.Lerp(cliffMinTime, cliffMaxTime, r * r);
     }
+  }
 }
