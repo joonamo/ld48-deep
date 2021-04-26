b73df9541e3c08aed9b59f570d3640145c4f785b
Joonamo <joonamo@users.noreply.github.com>
2021-04-26 17:31:55 +0300

    Smooth scrolling background!

---
+++ Assets/Objects/Scroller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour
+{
+  public float scrollTime = 10.0f;
+  public float scrollDistance = 18.0f;
+
+  GameManager gm;
+
+  Vector3 startPos;
+  Vector3 endPos;
+  float startTime = 0.0f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    gm = GameObject.FindObjectOfType<GameManager>();
+
+    this.startPos = transform.position;
+    this.endPos = startPos + Vector3.up * scrollDistance;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (gm.state == GameState.game) {
+      float phase = ((Time.time - startTime) % scrollTime) / scrollTime;
+      transform.position = Vector3.Lerp(startPos, endPos, phase);
+    } else {
+      startTime = Time.time;
+    }
+  }
+}
